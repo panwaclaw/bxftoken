b4037f746326c4911e7227db20f7fbddc0b16897
panwaclaw <panwaclaw@blackfort.group>
2020-12-05 01:46:18 +0300

    test update with new method names

--- test/TestReferral.js
+++ test/TestReferral.js
@@ -5,13 +5,11 @@ contract("BXFToken", accounts => {
         let instance = await BXFToken.deployed();
         let isCreated = [];
         console.log(accounts);
-        console.log(accounts[0]);
-        await instance.registerAccount(accounts[0], "0x0000000000000000000000000000000000000000", {from: accounts[0]});
-        isCreated[0] = await instance.hasRegistered(accounts[0]);
+        await instance.registerAccount("0x0000000000000000000000000000000000000000", {from: accounts[0]});
+        isCreated[0] = await instance.hasAccount(accounts[0]);
         for (let i = 1; i < accounts.length; i++) {
-          await instance.registerAccount(accounts[i], accounts[0], {from: accounts[i]});
-          isCreated[i] = await instance.hasRegistered(accounts[0]);
-          console.log(isCreated);
+          await instance.registerAccount(accounts[0], {from: accounts[i]});
+          isCreated[i] = await instance.hasAccount(accounts[i]);
         }
         for (let i = 0; i < isCreated.length; i++) assert.equal(isCreated[i], true, i.toString() + " not created");
     })
