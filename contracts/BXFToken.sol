75c8c34a63bf721432bfcb62aaa888774e3405d9
panwaclaw <panwaclaw@blackfort.group>
2021-01-04 02:27:14 +0300

    BXFTokenBase.sol removed

--- contracts/BXFToken.sol
+++ contracts/BXFToken.sol
@@ -4,10 +4,13 @@ pragma solidity ^0.7.5;
 pragma abicoder v2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "./BXFTokenBase.sol";
+import "./Company.sol";
+import "./Staking.sol";
+import "./Founder.sol";
+import "./Sale.sol";
 
 
-contract BXFToken is BXFTokenBase {
+contract BXFToken is Staking, Company, Sale, DirectBonus {
 
     using SafeMath for uint256;
     
@@ -97,5 +100,35 @@ contract BXFToken is BXFTokenBase {
 
         emit Transfer(sender, recipient, taxedTokens);
         return true;
+    }
+
+
+    function purchaseTokens(address senderAccount, uint256 amountOfEthereum) internal canInvest(amountOfEthereum) returns(uint256, uint256) {
+        uint256 taxedEthereum = amountOfEthereum;
+
+        uint256 companyFee = calculateCompanyFee(amountOfEthereum);
+        uint256 directBonus = calculateDirectBonus(amountOfEthereum);
+        uint256 stakingFee = calculateStakingFee(amountOfEthereum);
+
+        taxedEthereum = taxedEthereum.sub(companyFee);
+        increaseCompanyBalance(companyFee);
+
+        address account = senderAccount;
+        address sponsor = sponsorOf(account);
+        increaseSelfBuyOf(account, amountOfEthereum);
+
+        if (isEligibleForDirectBonus(sponsor)) {
+            addDirectBonusTo(sponsor, directBonus);
+            taxedEthereum = taxedEthereum.sub(directBonus);
+        }
+
+        taxedEthereum = taxedEthereum.sub(stakingFee);
+
+        uint256 amountOfTokens = ethereumToTokens(taxedEthereum);
+
+        processStakingOnBuy(senderAccount, amountOfTokens, stakingFee);
+        increaseBalanceOf(senderAccount, amountOfTokens);
+
+        return (taxedEthereum, amountOfTokens);
     }
 }
